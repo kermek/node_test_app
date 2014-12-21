540b83f59b6b7e698137c2e204f5bc2714dfa69f
kermek <kek277@gmail.com>
2014-12-21 13:55:19 +0200

    origin added

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var express = require('express'),
     app = express(),
+    path = require('path'),
     port = 8080;
 
-app.get('*', function(req, res) {
-  res.end('Hello world!');
+//set views
+//engine html
+console.log(path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function(req, res) {
+  res.render('index.html', { who: 'World' });
 });
 
 app.listen(port);
