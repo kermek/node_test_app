2f5134a1764918b37ed293fae0bc2e4604b7eb51
kermek <kek277@gmail.com>
2014-12-21 15:23:16 +0200

    login and logout

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express'),
     app = express(),
     path = require('path'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    cookieParser = require('cookie-parser'),
+    passport = require('./auth-middleware'),
     port = 8080;
 
 //set views
@@ -9,9 +13,44 @@ console.log(path.join(__dirname, 'views'));
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 
-app.get('/', function(req, res) {
-  res.render('index.html', { who: 'World' });
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'keyboard cat'
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', ensureAuthenticated,function(req, res) {
+  res.render('index.html', { user: req.user });
+});
+
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/login');
+});
+
+app.get('/login', function(req, res) {
+  res.render('login.html', { user: req.user });
+});
+
+app.post('/login',
+  function(req, res, next) {
+    console.log(req.body);
+    next();
+  },
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
 });
 
 app.listen(port);
 console.log('Server is up on poty %s!', port);
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+}
